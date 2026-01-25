4281115b0588a4c0da26c4a3aac709a401cf20d8
SimonDdungu <dev.simonddungu@gmail.com>
2026-01-25 14:34:47 +0300

    Pagination

--- quickmeds_backend/settings.py
+++ quickmeds_backend/settings.py
@@ -155,14 +155,17 @@ CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS") == "True"
 
 # from datetime import timedelta
 
-# REST_FRAMEWORK = {
+REST_FRAMEWORK = {
 #     "DEFAULT_AUTHENTICATION_CLASSES": (
 #         "rest_framework_simplejwt.authentication.JWTAuthentication",
 #     ),
 #     "DEFAULT_PERMISSION_CLASSES": (
 #         "rest_framework.permissions.IsAuthenticated",
 #     ),
-# }
+
+    "DEFAULT_PAGINATION_CLASS": 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
 
 # SIMPLE_JWT = {
 #     "ACCESS_TOKEN_LIFETIME": timedelta(
