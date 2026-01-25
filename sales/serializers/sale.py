425e78eca94eef1ab4b01b0267fa0194ab54ad28
SimonDdungu <dev.simonddungu@gmail.com>
2026-01-25 16:58:43 +0300

    Updated Sale Item to update Batch Quantity

--- sales/serializers/sale.py
+++ sales/serializers/sale.py
@@ -1,17 +1,20 @@
 from rest_framework import serializers
+from django.db import transaction
 from sales.models import Sale
 from users.serializers import UserSerializer
 from sales.serializers.sale_item import SaleItemSerializer
 
 class SaleSerializer(serializers.ModelSerializer):
     items = SaleItemSerializer(many=True)
-    soldby = UserSerializer(read_only=True)  
+    soldby = UserSerializer(read_only=True)
+    
     
     class Meta:
         model = Sale
         fields = "__all__"
         read_only_fields = ["id", "sold_by", "sold_at", "total_amount", 'updated_at']
-        
+     
+    @transaction.atomic 
     def create(self, validated_data):
         all_items = validated_data.pop("items")
         sale = Sale.objects.create(**validated_data)
@@ -19,10 +22,20 @@ class SaleSerializer(serializers.ModelSerializer):
         total = 0
         
         for each_item in all_items:
+            
+            batch = each_item["batch"]
+            quantity_sold = each_item["quantity"]
+            
+            if quantity_sold > batch.quantity_remaining:
+                raise serializers.ValidationError("Not Enough Stock")
+            
+            batch.quantity_remaining -= quantity_sold
+            batch.save()
+            
             each_item["sale"] = sale
             SaleItemSerializer().create(each_item)
             
-            total += each_item["quantity"] * each_item["price"]
+            total += quantity_sold * each_item["unit_price"]
             
         sale.total_amount = total
             
