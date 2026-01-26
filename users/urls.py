cbd8b56dd3ae4ba58401c0a3127a9f5daec51e3b
SimonDdungu <dev.simonddungu@gmail.com>
2026-01-26 15:19:15 +0300

    Updated Token Urls

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet, PasswordResetConfirmView, PasswordResetRequestView
 
@@ -7,7 +8,9 @@ router.register(r'', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('password-reset/', PasswordResetRequestView.as_view()),
-    path('password-reset-confirm/', PasswordResetConfirmView.as_view()),
+    path('api/auth/password-reset/', PasswordResetRequestView.as_view()),
+    path('api/auth/password-reset-confirm/', PasswordResetConfirmView.as_view()),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
