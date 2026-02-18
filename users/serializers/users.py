93b111b9e0c6df1dc0b8b02d9522894914ba8dcb
SimonDdungu <dev.simonddungu@gmail.com>
2026-02-19 01:21:35 +0300

    Lost track of changes

--- users/serializers/users.py
+++ users/serializers/users.py
@@ -5,12 +5,13 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     groups = serializers.SlugRelatedField(many=True,read_only=True, slug_field='name')
 
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, required=False)
     role = serializers.CharField(write_only=True)
+    
     class Meta:
         model = User
-        fields = '__all__'
-        read_only_fields = ['id', 'created_at', 'updated_at']
+        fields = ['id', 'first_name', 'last_name', 'username', 'groups', 'email', 'phone_number', 'gender', 'profile_image', 'role', 'password', 'is_active', 'is_superuser', 'groups', 'created_at', 'updated_at',]
+        read_only_fields = ['id', 'groups', 'created_at', 'updated_at']
         
     def create(self, validated_data):
         password = validated_data.pop('password')
@@ -29,3 +30,20 @@ class UserSerializer(serializers.ModelSerializer):
         user.groups.add(group)
         return user
             
+            
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+
+        role = validated_data.pop('role', None)
+        if role:
+            group = Group.objects.get(name=role)
+            instance.groups.clear()
+            instance.groups.add(group)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
--- users/views/user.py
+++ users/views/user.py
@@ -1,3 +1,4 @@
+import logging
 from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from users.filters import UserFilterSet
@@ -5,12 +6,16 @@ from users.permissions import UserAuth
 from users.models import User
 from users.serializers import UserSerializer
 
+logger = logging.getLogger(__name__)
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     #permission_class = [UserAuth]
-    
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_class = UserFilterSet
     ordering_fields = ['created_at', 'updated_at', 'username', 'first_name', 'last_name']
     
+    def handle_exception(self, exc):
+        logger.error(f"User Error: {exc}")
+        return super().handle_exception(exc)
