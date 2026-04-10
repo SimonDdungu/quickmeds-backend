feaafe3f874a447404dd1218c1ad9149343e34f5
SimonDdungu <dev.simonddungu@gmail.com>
2026-04-11 02:54:56 +0300

    Pray and Hope

--- quickmeds_backend/settings.py
+++ quickmeds_backend/settings.py
@@ -211,16 +211,17 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
         },
     },
-
-    "root": {
-        "handlers": ["console"],
-        "level": "ERROR",
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
     },
 }
 
